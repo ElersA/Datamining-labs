b7dd92ef00fbafce0c1fc0a83cf2086ddde07c33
ElersA <elers93@gmail.com>
2018-11-02 08:39:32 +0100

    minHashing done

--- lab1/src/main/scala/SimilarItems.scala
+++ lab1/src/main/scala/SimilarItems.scala
@@ -1,81 +1,59 @@
 import scala.collection.SortedSet
+import scala.collection.mutable.ListBuffer
 import scala.io.Source
 import scala.util.Random
 
 object SimilarItems {
 
   // For testing
-  val primes: List[Long] = Source.fromFile("./primes").getLines.toList.map(_.toLong) // From https://www.bigprimes.net/archive/prime/13999999/
+  val primes: List[Int] = Source.fromFile("./primes").getLines.toList.map(_.toInt) // From https://www.bigprimes.net/archive/prime/13999999/
   def readFile: List[Char] = Source.fromFile("./tempData.txt").getLines.toList.flatten
 
-  def shingling(k: Int, document: List[Char]): SortedSet[Long] = {
+  def shingling(k: Int, document: List[Char]): SortedSet[Int] = {
     val kShingles: Set[List[Char]] = document.sliding(k).toSet
     val hashedShingles = kShingles.map(_.hashCode()) // TODO maybe use abs to avoid negative numbers
     SortedSet[Int]() ++ hashedShingles // Convert our Set to a SortedSet
   }
 
-  def compareSets(a: SortedSet[Long], b: SortedSet[Long]): Double = {
+  def compareSets(a: SortedSet[Int], b: SortedSet[Int]): Double = {
     val numerator = a.intersect(b).size
     val denominator = a.union(b).size
 
     numerator / denominator match {
       case 0 => throw new IllegalArgumentException("Do not divide by zero")
-      case _ => numerator / denominator
+      case _ => numerator.toDouble / denominator.toDouble
     }
   }
 
-  def minHashing(n: Int, hashedShingles: SortedSet[Long]): Set[Long] = {
-     val rnd = new Random
-    val listOfMin = new ListBuffer[Int]()
+  def minHashing(n: Int, hashedShingles: SortedSet[Int]): Set[Int] = {
+    val rnd = new Random
     rnd.setSeed(0)
     var minval = Int.MaxValue
+    val listOfMin = new ListBuffer[Int]()
     // foreach hash apply to every shingle get min value for that hash and save to list
-    primes.foreach(prime =>{
-        val a = rnd.nextInt(10)
-        val b = rnd.nextInt(10)
-        hashedShingles.foreach(shingle=>{
-          var tempmin = (a*shingle+b) % prime
-          if(minval < tempmin) {
-            minval = tempmin
-          }
-        })
-      listOfMin.+=(minval)
-      minval = Int.MaxValue
-      }
-    )
-
-    
-    primes map {prime =>
-      var tempMin = hashFunc(prime)(hashedShingles.head) // Initial value
-      val hashFuncWithPrime = hashFunc(prime)
-
-      hashedShingles.fold(tempMin){ (acc, elem) =>
-        if (hashFuncWithPrime(elem) < acc) elem else acc
-      }
-
-    }
-
-    // Maybe use for expression!
-
-    for {
-      prime <- primes
-      min <- getMin
-    } yield min
-
-
-    def hashFunc(prime: Long)(hashedShingle: Long): Long = {
+    primes.take(n).foreach { prime =>
       /*
         hash function: (ax+b) % c
           a and b are random values
           c is a large prime.
           x is an element from the input set
       */
-      val rnd = new Random
-      rnd.setSeed(0)
-      val a = rnd.nextLong * hashedShingle
-      val b = rnd.nextLong * hashedShingle
-      (a * hashedShingle) % prime
+      val a = rnd.nextInt(10)
+      val b = rnd.nextInt(10)
+      hashedShingles.foreach { shingle =>
+        var tempmin = (a * shingle + b) % prime
+        if (minval < tempmin) {
+          minval = tempmin
+        }
+      }
+      listOfMin.append(minval)
+      minval = Int.MaxValue
     }
+    listOfMin.toSet
+  }
+
+  def compareSignatures(a: Set[Int], b: Set[Int]): Double = {
+
   }
 
   def main(args: Array[String]): Unit = {
