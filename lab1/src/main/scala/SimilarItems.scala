be34de74d30b6d32162f7b895eb464f59bb13294
adahm <18390656+adahm@users.noreply.github.com>
2018-11-02 13:04:18 +0100

    added comments and somecode to tester

--- lab1/src/main/scala/SimilarItems.scala
+++ lab1/src/main/scala/SimilarItems.scala
@@ -9,22 +9,26 @@ object SimilarItems {
   val primes: List[Int] = Source.fromFile("./primes").getLines.toList.map(_.toInt) // From https://www.bigprimes.net/archive/prime/13999999/
   def readFile: List[Char] = Source.fromFile("./tempData.txt").getLines.toList.flatten
 
+  //create shinglings of length k from the list of charachters and return a sorted set contating the hased values for the shinglings
   def shingling(k: Int, document: List[Char]): SortedSet[Int] = {
     val kShingles: Set[List[Char]] = document.sliding(k).toSet
     val hashedShingles = kShingles.map(shingle => Math.abs(shingle.hashCode()))
     SortedSet[Int]() ++ hashedShingles // Convert our Set to a SortedSet
   }
 
+  //Return the Jacard similarty of two sets
   def compareSets(a: SortedSet[Int], b: SortedSet[Int]): Double = {
     val numerator = a.intersect(b).size
     val denominator = a.union(b).size
 
+    //Check that both sets are not 0
     numerator / denominator match {
       case 0 => throw new IllegalArgumentException("Do not divide by zero")
       case _ => numerator.toDouble / denominator.toDouble
     }
   }
 
+  //Get the minash for the shingles
   def minHashing(n: Int, hashedShingles: SortedSet[Int]): Set[Int] = {
     val rnd = new Random
     rnd.setSeed(0)
@@ -38,8 +42,9 @@ object SimilarItems {
           c is a large prime.
           x is an element from the input set
       */
-      val a = rnd.nextInt(10)
-      val b = rnd.nextInt(10)
+      val a = 3
+      val b = 5
+      //For each shinle apply the hashfunction save if it is less than what is saved
       hashedShingles.foreach { shingle =>
         var tempmin = (a * shingle + b) % prime
         if (minval < tempmin) {
@@ -49,26 +54,35 @@ object SimilarItems {
       listOfMin.append(minval)
       minval = Int.MaxValue
     }
+    //Return a set of miniumhashvalues
     listOfMin.toSet
   }
 
+  //Return the fraction from the amount of hashes equal in two sets devieded by the amount of hashes used.
   def compareSignatures(a: Set[Int], b: Set[Int]): Double = {
     // Assumption: a & b are of equal size
     val count = a.zip(b).foldLeft(0)((acc: Int, hashTuple: (Int, Int)) => if (hashTuple._1 == hashTuple._2) acc + 1 else acc)
     count.toDouble / a.size.toDouble
   }
 
-  def LSH(minHashes: List[Set[Int]], similarity: Double): List[(Int, Int)] = {
+  //Return the canditate pairs that will be then compared for signatures.
+  def LSH(signatures: List[Set[Int]], similarity: Double): List[(Int, Int)] = {
     val b = 20 // Bands. Hardcoded values from the slides
     val r = 5 // Rows. Hardcoded values from the slides
 
     val buckets = 10000
     val candidatePairHolder: Map[Int, Map[Int, List[Int]]] = Map.empty
 
-    minHashes foreach {hashes =>
-      hashes.sliding(r, r).sum
+    //for each signature get the sum of the band and hash it
+    //place the document id into the bucket from the hash
+    signatures foreach {sig =>
+      sig.sliding(r, r).map(_.sum).toList
 
     }
+
+
+    //Iterate over the buckets and create pairs of documents
+    //return the pairs of documents
 
     /*
         1. Iterate over each Set in minHashes
--- lab1/src/main/scala/Tester.scala
+++ lab1/src/main/scala/Tester.scala
@@ -1,7 +1,11 @@
 
 import SimilarItems._
+
 import scala.io.Source
 import java.io.File
+
+import scala.collection.SortedSet
+import scala.collection.mutable.ListBuffer
 
 object Tester extends App {
 
@@ -13,6 +17,33 @@ object Tester extends App {
   // TODO remake
   val documents: List[List[Char]] = readDocuments("./documents")
   val similarity = 0.8
+  //List of documents hashedshingles
+  val listOfShinglings = new ListBuffer[SortedSet[Int]]()
+  var count =0
+  //foreach document get the sortedset containing the hashedshingles
+  documents.foreach{doc =>
+    listOfShinglings.append(SimilarItems.shingling(9,doc))
+  }
+
+  //compare the documents and print the docus with a jacardsimilarity over 0.8
+
+  SimilarItems.compareSets()
+
+  //List of doucment signatures
+  val listOfSignatures = new ListBuffer[Set[Int]]()
+  //convert the hashed shingles to signatures
+  listOfShinglings.foreach{shing =>
+    listOfSignatures.append(SimilarItems.minHashing(100,shing))
+  }
+
+  //compare the signatures of the doucments
+  //print doucment that are similar >0.8
+
+  //apply lsh on the signatures
+
+  //take the list of suggested pairs and compare their signatures.
+  //print document that are similar >0.8
+
 
 
   /* Code to measure time
