0b336e0e48a704db619f0e046b8c0817d5cb9c24
ElersA <elers93@gmail.com>
2018-11-20 10:17:09 +0100

    Adding scaffolding code for reservoir sampling

--- lab3/src/main/scala/PaperImplementation.scala
+++ lab3/src/main/scala/PaperImplementation.scala
@@ -13,7 +13,28 @@ object PaperImplementation extends App{
   // Use the config to create a streaming context with a batch interval of every 5 seconds.
   val ssc = new StreamingContext(sparkConf, Seconds(5))
 
+  val M_threshold = 6
+  val random = scala.util.Random
+  random.setSeed(0L)
+  var S: scala.collection.mutable.Set[(Int, Int)] = Set[(Int, Int)]()
 
 
+
+  def reservoirSampling(nodeAndEdge: (Int, Int), t: Int): Boolean = {
+    if (t <= M_threshold) {
+      true
+    } else if (random.nextDouble() <= M_threshold.toDouble / t) {
+      val toRemove = S.toVector(random.nextInt(M_threshold))
+      S.remove(toRemove)
+      updateCounters(-1, (1,2))
+      true
+    }
+    false
+  }
+
+  // Mode can be either 1 for addition or -1 for subtraction
+  def updateCounters(mode: Int, nodeAndEdge: (Int, Int)): Unit = {
+
+  }
 
 }
