25e80a1cdaf764aa6c00df55b10db58b75501202
ElersA <elers93@gmail.com>
2018-11-21 14:16:06 +0100

    Changed representation to Set[(Int, Int)] instead. Potentially working solution.

--- lab3/src/main/scala/PaperImplementation.scala
+++ lab3/src/main/scala/PaperImplementation.scala
@@ -2,53 +2,108 @@
 import scala.collection.mutable
 import scala.io.Source
 
-object PaperImplementation extends App{
+object PaperImplementation {
 
-  var M_threshold = 0
   val random = scala.util.Random
   random.setSeed(0L)
-  var sample: mutable.Map[Int, (Int, Set[Int])] = mutable.Map[Int, (Int, Set[Int])]()
+  var counters: mutable.Map[Int, Int] = mutable.Map[Int, Int]()
+  var sample: mutable.Set[(Int, Int)] = mutable.Set[(Int, Int)]()
   var T = 0 // Counter for number of global triangles
   var iteration = 0
-  val dataStream: Iterator[String] = readData("./src/main/scala/data/REPLACE THIS") // TODO replace with a file
 
-  dataStream
-    .map{twoNodes =>
-      val nodes = twoNodes.split(" ")
-      (nodes(0).toInt, nodes(1).toInt)
+  var M_threshold = 15000
+  val isTest = false
+
+  def readData(isTest: Boolean): Iterator[String] = {
+    if (isTest) {
+      Source.fromFile("./src/main/scala/data/p2p-Gnutella06.txt").getLines
+    } else {
+      Source.fromFile("./src/main/scala/data/validationGraph").getLines
+      //Source.fromFile("./src/main/scala/data/actor-collaboration/out.actor-collaboration").getLines
     }
-    .foreach{nodes =>
-    iteration += 1
-    if (reservoirSampling(nodes, iteration)) {
-      sample.put(nodes._1, (0, Set(nodes._2)))
-      updateCounters(1, nodes)
-    }
+  }
+  def main(args: Array[String]): Unit = {
+    val dataStream = readData(isTest)
+
+    dataStream
+      .filterNot(line => line.startsWith("#") || line.startsWith("%")) // the test dataset contains some comments in the beginning, we don't want those
+      .map { twoNodes =>
+        val nodes = if (isTest) twoNodes.split("\t") else twoNodes.split(" ")
+        (nodes(0).toInt, nodes(1).toInt)
+      }
+      .foreach { nodes =>
+        iteration += 1
+
+        if (reservoirSampling(nodes, iteration)) {
+          sample.+=((nodes._1, nodes._2))
+          updateCounters(1, nodes)
+        }
+      }
+    //println(s"Number of counters: ${counters.size}")
+    //printCounters()
   }
 
   def reservoirSampling(nodes: (Int, Int), t: Int): Boolean = {
     if (t <= M_threshold) {
       true
     } else if (random.nextDouble() <= M_threshold.toDouble / t) {
-      val keys = sample.keySet.toList
-      val randomKey = random.nextInt(keys.size)
-      val keyToRemove = keys(randomKey)
-      sample.remove(keyToRemove)
-      updateCounters(-1, (1,2))
+      val sampleAsVector = sample.toVector
+      val elementToRemove = sampleAsVector(random.nextInt(sampleAsVector.length)) // Get a random element
+      sample.remove(elementToRemove)
+      counters.remove(elementToRemove._1)
+      updateCounters(-1, elementToRemove) // Decrement counters for removed nodes
       true
+    } else {
+      false
     }
-    false
   }
 
   // Mode can be either 1 for addition or -1 for subtraction
   def updateCounters(mode: Int, nodes: (Int, Int)): Unit = {
-    val commonNeighbourhood = sample(nodes._1)._2.intersect(sample(nodes._2)._2)
-    commonNeighbourhood.foreach{commonNode =>
+    //println(s"Sample: $sample")
+    //println(s"nodes: $nodes")
+
+    val nodeOnesNeighbors = sample.collect{
+      case (a, b) if a == nodes._1 => b
+      case (a, b) if b == nodes._1 => a
+    }.toSet
+    val nodeTwosNeighbors = sample.collect{
+      case (a, b) if a == nodes._2 => b
+      case (a, b) if b == nodes._2 => a
+    }.toSet
+
+    //println(s"nodeOnesNeighbors = $nodeOnesNeighbors\nnodeTwosNeighbors = $nodeTwosNeighbors")
+
+    val commonNeighbourhood = nodeOnesNeighbors.intersect(nodeTwosNeighbors)
+    //println(s"Neighbourhood: $commonNeighbourhood")
+
+    commonNeighbourhood.foreach { commonNode =>
       T = T + mode
-      sample(commonNode) = (sample(commonNode)._1 + mode, sample(commonNode)._2) // TODO check if we need options
-      sample(nodes._1) = (sample(nodes._1)._1 + mode, sample(nodes._1)._2)
-      sample(nodes._2) = (sample(nodes._2)._1 + mode, sample(nodes._2)._2)
+
+      val commonNodeCounter = counters.getOrElse(commonNode, 0)
+      val nodeOneCounter = counters.getOrElse(nodes._1, 0)
+      val nodeTwoCounter = counters.getOrElse(nodes._2, 0)
+
+      counters.update(commonNode, commonNodeCounter + mode)
+      counters.update(nodes._1, nodeOneCounter + mode)
+      counters.update(nodes._2, nodeTwoCounter + mode)
+
+      if (commonNodeCounter == 1 && mode == -1) {
+        counters.remove(commonNode)
+      } else if (nodeOneCounter == 1 && mode == -1) {
+        counters.remove(nodes._1)
+      } else if (nodeOneCounter == 1 && mode == -1) {
+        counters.remove(nodes._2)
+      }
     }
   }
 
-  def readData(path: String): Iterator[String] = Source.fromFile(path).getLines
+  def printCounters(): Unit = {
+    println("--- Counters ---")
+    println(s"Global triangles: $T")
+    /*
+    counters.foreach{case (key, counter) =>
+      println(s"$key has $counter triangles")
+    }*/
+  }
 }
