5d31966655b3b6eb45c04edf551637ddc7fac72a
ElersA <elers93@gmail.com>
2018-11-21 14:55:37 +0100

    Removing dependencies in build.sbt, adding validationGraph and initial testing shows correct results.

--- lab3/build.sbt
+++ lab3/build.sbt
@@ -2,9 +2,4 @@ name := "lab3"
 
 version := "0.1"
 
-scalaVersion := "2.11.8"
-
-libraryDependencies ++= Seq(
-  "org.apache.spark" %% "spark-core" % "2.2.1",
-  "org.apache.spark" % "spark-streaming_2.11" % "2.2.1",
-)
+scalaVersion := "2.12.7"
--- lab3/src/main/scala/PaperImplementation.scala
+++ lab3/src/main/scala/PaperImplementation.scala
@@ -8,10 +8,10 @@ object PaperImplementation {
   random.setSeed(0L)
   var counters: mutable.Map[Int, Int] = mutable.Map[Int, Int]()
   var sample: mutable.Set[(Int, Int)] = mutable.Set[(Int, Int)]()
-  var T = 0 // Counter for number of global triangles
-  var iteration = 0
+  var T: Double = 0 // Counter for number of global triangles
+  var t = 0 // Edge counter
 
-  var M_threshold = 15000
+  var M_threshold = 6
   val isTest = false
 
   def readData(isTest: Boolean): Iterator[String] = {
@@ -22,28 +22,28 @@ object PaperImplementation {
       //Source.fromFile("./src/main/scala/data/actor-collaboration/out.actor-collaboration").getLines
     }
   }
+
   def main(args: Array[String]): Unit = {
     val dataStream = readData(isTest)
 
     dataStream
       .filterNot(line => line.startsWith("#") || line.startsWith("%")) // the test dataset contains some comments in the beginning, we don't want those
-      .map { twoNodes =>
-        val nodes = if (isTest) twoNodes.split("\t") else twoNodes.split(" ")
-        (nodes(0).toInt, nodes(1).toInt)
+      .map{twoNodes =>
+      val nodes = if (isTest) twoNodes.split("\t") else twoNodes.split(" ")
+      (nodes(0).toInt, nodes(1).toInt)
       }
       .foreach { nodes =>
-        iteration += 1
-
-        if (reservoirSampling(nodes, iteration)) {
+        t += 1
+        updateCounters(nodes)
+        if (reservoirSampling(nodes)) {
           sample.+=((nodes._1, nodes._2))
-          updateCounters(1, nodes)
         }
       }
     //println(s"Number of counters: ${counters.size}")
-    //printCounters()
+    printResults()
   }
 
-  def reservoirSampling(nodes: (Int, Int), t: Int): Boolean = {
+  def reservoirSampling(nodes: (Int, Int)): Boolean = {
     if (t <= M_threshold) {
       true
     } else if (random.nextDouble() <= M_threshold.toDouble / t) {
@@ -51,43 +51,40 @@ object PaperImplementation {
       val elementToRemove = sampleAsVector(random.nextInt(sampleAsVector.length)) // Get a random element
       sample.remove(elementToRemove)
       counters.remove(elementToRemove._1)
-      updateCounters(-1, elementToRemove) // Decrement counters for removed nodes
       true
     } else {
       false
     }
   }
 
-  // Mode can be either 1 for addition or -1 for subtraction
-  def updateCounters(mode: Int, nodes: (Int, Int)): Unit = {
-    //println(s"Sample: $sample")
-    //println(s"nodes: $nodes")
+  def updateCounters(nodes: (Int, Int)): Unit = {
 
-    val nodeOnesNeighbors = sample.collect{
+    val nodeOnesNeighbors = sample.collect {
       case (a, b) if a == nodes._1 => b
       case (a, b) if b == nodes._1 => a
     }.toSet
-    val nodeTwosNeighbors = sample.collect{
+    val nodeTwosNeighbors = sample.collect {
       case (a, b) if a == nodes._2 => b
       case (a, b) if b == nodes._2 => a
     }.toSet
 
-    //println(s"nodeOnesNeighbors = $nodeOnesNeighbors\nnodeTwosNeighbors = $nodeTwosNeighbors")
-
     val commonNeighbourhood = nodeOnesNeighbors.intersect(nodeTwosNeighbors)
-    //println(s"Neighbourhood: $commonNeighbourhood")
 
     commonNeighbourhood.foreach { commonNode =>
-      T = T + mode
+      println("Found triangle")
+      val increment = math.max(1, ((t - 1) * (t - 2)).toDouble / (M_threshold * (M_threshold - 1)))
+      T = T + increment
 
+      /*
       val commonNodeCounter = counters.getOrElse(commonNode, 0)
       val nodeOneCounter = counters.getOrElse(nodes._1, 0)
       val nodeTwoCounter = counters.getOrElse(nodes._2, 0)
 
-      counters.update(commonNode, commonNodeCounter + mode)
-      counters.update(nodes._1, nodeOneCounter + mode)
-      counters.update(nodes._2, nodeTwoCounter + mode)
-
+      counters.update(commonNode, commonNodeCounter + increment)
+      counters.update(nodes._1, nodeOneCounter + increment)
+      counters.update(nodes._2, nodeTwoCounter + increment)
+      */
+      /*
       if (commonNodeCounter == 1 && mode == -1) {
         counters.remove(commonNode)
       } else if (nodeOneCounter == 1 && mode == -1) {
@@ -95,11 +92,13 @@ object PaperImplementation {
       } else if (nodeOneCounter == 1 && mode == -1) {
         counters.remove(nodes._2)
       }
+      */
     }
   }
 
-  def printCounters(): Unit = {
-    println("--- Counters ---")
+  def printResults(): Unit = {
+    println(s"Number of edges: $t")
+    println(s"Global triangles: ${math.round(T)}")
     println(s"Global triangles: $T")
     /*
     counters.foreach{case (key, counter) =>
