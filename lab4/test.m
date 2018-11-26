5830b4692648f890d0816009cd85cc76920545b7
adahm <18390656+adahm@users.noreply.github.com>
2018-11-26 11:17:36 +0100

    added plots of graphs and the result from k-means

--- lab4/test.m
+++ lab4/test.m
@@ -1,9 +1,9 @@
 %Read the data
-E = csvread("./example2.dat");
-k = 2;
+%E = csvread("./example2.dat");
+%k = 2;
 
-%E = csvread("./example1.dat")
-%k = 4
+E = csvread("./example1.dat")
+k = 4
 
 %Get the vertexs to column 1 and 2
 col1 = E(:,1);
@@ -21,39 +21,55 @@ A = full(As)
 %From this we can see that example1.dat has 4 modules and eample2.dat has 2
 %modules 
 G = graph(A,'omitselfloops');
-%plot(G)
+plot(G)
 
-%f = [2,3;4,1]
+f = [2,3;4,1]
 
-D = diag(sum(f,2));
+D = diag(sum(A,2));
 
 
 %Step 2
-L = (D^(-0.5)).*A.*(D^(-0.5));
-%L = (D^(-0.5)).*f.*(D^(-0.5))
+L = (D^(-0.5))*A*(D^(-0.5));
 
 %step 3 http://matlab.izmiran.ru/help/techdoc/ref/eigs.html
-%[Svec,Fval] = eig(L)
+%[Svec,Fval] = eig(L);
+[X,S] = eigs(L,k)
 
-%X = eigs(Svec,k)
 
-%nEigVec = Svec(:,(size(Svec,1)-(k-1)): size(Svec,1));
 %Step 4 renomrmalize X to Y
 %https://se.mathworks.com/help/matlab/matlab_prog/array-vs-matrix-operations.html
 %. for element wise operations
 %S = sum(A,dim) returns the sum along dimension dim. For example, if A is a matrix, then sum(A,2) is a column vector containing the sum of each row.
 %https://se.mathworks.com/help/matlab/ref/sum.html
-%Y = X./sqrt(sum(X.^2,2))
+Y = X./sqrt(sum(X.^2,2))
 
 %https://se.mathworks.com/help/stats/kmeans.html
 %step 5 use k-means in matlab
-%Idx = kmeans(Y,k)
+Idx = kmeans(Y,k)
 
 %Step 6
 %orignal points in A
 % if row i in Y was assinged to 
 % https://se.mathworks.com/help/matlab/ref/hold.html hold of
 % https://se.mathworks.com/help/matlab/ref/figure.html ploting points
+
+figure,
+hold on;
+
+for i=1:size(Idx,1)
+    if Idx(i,1) == 1
+        plot(i,1,'kx');
+    elseif Idx(i,1) == 2
+        plot(i,2,'kx');
+    elseif Idx(i,1) == 3
+        plot(i,3,'kx');   
+    elseif Idx(i,1) == 4
+        plot(i,4,'kx');   
+    end
+end
+hold off;
+title('Test graph');
+grid on;shg
 
 
 
